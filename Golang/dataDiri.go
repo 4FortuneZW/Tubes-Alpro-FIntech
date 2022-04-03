a46ccbaa097f2fa310ec5436a3b09b343cafe516
Draxterra <93527194+Draxterra@users.noreply.github.com>
2022-04-03 22:48:35 +0700

    Update dataDiri.go

--- Golang/dataDiri.go
+++ Golang/dataDiri.go
@@ -1,34 +1,26 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
 
 type User struct {
 	Nama, JenisKelamin string
-	usia int
-	saldo float64
+	usia               int
+	saldo              float64
 }
+
 const NMAX = 30
 
-type tabUser  [NMAX]User
+type tabUser [NMAX]User
 
 func DataDiri(DtUser *tabUser, n *int) {
 	var nama string
 	var usia int
 	var saldo float64
 	var JenisKelamin string
-	
-	
-	fmt.Scanln(&n)
-	i := 0
-	for i < n && *DtUser <= NMAX-1 {
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan() // use `for scanner.Scan()` to keep reading
-		nama := scanner.Text()
-		fmt.Println("Nama :", nama)
+
+	for i := 0; i < *n; i++ {
+		fmt.Print("Nama : ")
+		fmt.Scanln(&nama, &nama, &nama)
 		fmt.Print("Usia : ")
 		fmt.Scanln(&usia)
 		fmt.Print("Jenis Kelamin : ")
@@ -36,20 +28,18 @@ func DataDiri(DtUser *tabUser, n *int) {
 		fmt.Print("Saldo : ")
 		fmt.Scanln(&saldo)
 
-		tabUser[i].Nama = nama
-		tabUser[i].Usia = usia
-		tabUser[i].Jenis_Kelamin = JenisKelamin
-		tabUser[i].Saldo = saldo
-		*DtUser++
-		i++
-}  
-
-
+		DtUser[i].Nama = nama
+		DtUser[i].usia = usia
+		DtUser[i].JenisKelamin = JenisKelamin
+		DtUser[i].saldo = saldo
+	}
+}
 
 func main() {
-	var Datadiri tabUser
-	var n,
-	
+	var DtUser tabUser
+	var n int
 
-
+	fmt.Print("Jumlah orang : ")
+	fmt.Scanln(&n)
+	DataDiri(&DtUser, &n)
 }
